a938f1767cfa9f8a83b8317fa7e16b14cfb3bf57
David Martin <david@itbeyond.com.au>
2012-06-25 11:43:56 +0800

    Added options contentType, encodeValue, full POST json support and support URL as value
    ,
    
    Added the contentType ajax option defaults to
    'application/x-www-form-urlencoded' can be changed to 'application/json;
    charset=utf-8' which is required for .net web service support. To make
    this work the response must be changed for data.d - this has been added.
    
    Also to support the use of a URL as the returned value the
    encodeURIComponent in the buildItems function must be able disabled, so
    the option encodeValue was added this defaults to true which means the
    value is encoded - set to false to support URL or non encode required
    values.

--- jqm.autoComplete-1.4.js
+++ jqm.autoComplete-1.4.js
@@ -13,6 +13,7 @@
 
 	var defaults = {
 		method: 'GET',
+	        contentType: 'application/x-www-form-urlencoded',
 		icon: 'arrow-r',
 		cancelRequests: false,
 		target: $(),
@@ -20,17 +21,20 @@
 		callback: null,
 		link: null,
 		minLength: 0,
-		transition: 'fade'
+		transition: 'fade',
+	        encodeValue: true
 	},
 	openXHR = {},
 	buildItems = function($this, data, settings) {
-		var str = [];
+		var str = [], usevalue = '';
 		if (data) {
 			$.each(data, function(index, value) {
 				// are we working with objects or strings?
 				if ($.isPlainObject(value)) {
+		                	if (settings.encodeValue) usevalue = encodeURIComponent(value.value); else usevalue = value.value;
 					str.push('<li data-icon=' + settings.icon + '><a href="' + settings.link + encodeURIComponent(value.value) + '" data-transition="' + settings.transition + '">' + value.label + '</a></li>');
 				} else {
+					if (settings.encodeValue) usevalue = encodeURIComponent(value); else usevalue = value;
 					str.push('<li data-icon=' + settings.icon + '><a href="' + settings.link + encodeURIComponent(value) + '" data-transition="' + settings.transition + '">' + value + '</a></li>');
 				}
 			});
@@ -95,6 +99,7 @@
 						type: settings.method,
 						url: settings.source,
 						data: { term: text },
+						contentType: settings.contentType,
 						beforeSend: function(jqXHR) {
 							if (settings.cancelRequests) {
 								if (openXHR[id]) {
@@ -110,7 +115,7 @@
 							}
 						},
 						success: function(data) {
-							buildItems($this, data, settings);
+							if (data.d) buildItems($this, data.d, settings); else buildItems($this, data, settings);
 						},
 						complete: function (jqXHR, textStatus) {
 							// Clear this ID's open XML HTTP Request from the list
